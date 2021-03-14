bf6fb7772e97eae19cd853c968f9ca7f7859f8f2
hollaze <deperiers.a@gmail.com>
2021-03-14 16:53:36 +0000

    Adding variadic

--- printf_files/print_s.c
+++ printf_files/print_s.c
@@ -1,14 +1,29 @@
 #include "holberton.h"
+#include <stdarg.h>
 
 /**
  *
  *
+ *
+ *
  */
 
-void printf_s(char *s)
+int printf_s(va_list args)
 {
 	int i;
+	char *s;
 
-	for (i = 0; s != '\0'; i++)
+	if (!s)
+		return (1);
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s = va_arg(args, char *);
 		_putchar(s[i]);
+	}
+
+	s[i] = '\0';
+
+return (i);
 }
+
