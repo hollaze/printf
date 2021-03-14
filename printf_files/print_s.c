3f8b48c336303e0643bbc7fb789d1f7789a2e04e
hollaze <deperiers.a@gmail.com>
2021-03-14 10:59:03 +0000

    print_c.c

--- printf_files/print_s.c
+++ printf_files/print_s.c
@@ -5,18 +5,10 @@
  *
  */
 
-void printf_s(char *s, ...)
+void printf_s(char *s)
 {
-
 	int i;
 
-	va_list args;
-
-	va_start(args, s);
-
 	for (i = 0; s != '\0'; i++)
-		s = va_arg(args, char *)
-
-	va_end(args);
-
+		_putchar(s[i]);
 }
