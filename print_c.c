5455538130e03e91b731be2c25a3c643b7fecaac
hollaze <deperiers.a@gmail.com>
2021-03-17 10:10:15 +0000

    adding coms

--- print_c.c
+++ print_c.c
@@ -1,20 +1,20 @@
 #include "holberton.h"
 
 /**
-*print_c - function that print char
-@args: list of argument
-*Return : char
+* print_c - function that print char
+* @args: list of argument
+* Return: number of character - 1
 */
 
 int print_c(va_list args)
 {
-        int c;
+	int c;
 
 	if (!c)
 		return (-1);
 
-        c = va_arg(args, int);
-        _putchar(c);
+	c = va_arg(args, int);
+	_putchar(c);
 
-        return (1);
+	return (1);
 }
