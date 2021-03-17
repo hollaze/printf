5b186a1a159d974f6670882f3c73934dc5c8dd87
hollaze <deperiers.a@gmail.com>
2021-03-17 19:55:30 +0000

    adding condition

--- _printf.c
+++ _printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 	int i, count = 0;
 	int (*res)(va_list);
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	va_start(args, format);
