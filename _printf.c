699928ab3c006e55bff6210141e5c9eb6dea40a8
hollaze <deperiers.a@gmail.com>
2021-03-17 20:26:37 +0000

    handling betty

--- _printf.c
+++ _printf.c
@@ -19,37 +19,30 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] != '\0')
 				res = get_letter_func(format[i + 1]);
-
 			if (res == NULL)
 			{
 				_putchar(format[i]);
 				i++;
 				count++;
 			}
-
 			else
 			{
 				count += res(args);
 				i += 2;
 				continue;
 			}
-
 		}
 		_putchar(format[i]);
 		count++;
 		i++;
 	}
-
 	va_end(args);
-
 return (count);
-
 }
 
